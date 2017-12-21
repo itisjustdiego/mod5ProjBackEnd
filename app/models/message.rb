b8f9a87b35bbf8d7027cf76a69ad5ea5890ad114
itisjustdiego <itisjustdiego@gmail.com>
2017-12-21 11:44:48 -0500

    stuff

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,6 @@
 class Message < ActiveRecord::Base
-  # belongs_to :user
 
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
 
-  # belongs_to :sender, :class_name => 'User', :foreign_key => 'user_id'
-  # belongs_to :receiver, :class_name => 'User', :foreign_key => 'user_id'
 end
